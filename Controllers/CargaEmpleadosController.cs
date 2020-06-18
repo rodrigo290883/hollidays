1ae3b4277698fe877bee9fb81af485c6d328c514
francisco abraham ramos <abrahamram1291@gmail.com>
2020-06-18 14:08:20 -0500

    Subir Archivos Excel Debug

--- Controllers/CargaEmpleadosController.cs
+++ Controllers/CargaEmpleadosController.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Web;  
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using LinqToExcel;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using desconectate.Models;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.AspNetCore.Hosting;
+using desconetate.Models;
+
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,6 +34,22 @@ namespace desconectate.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
+        }
+
+        public IActionResult SubirArchivo(){
+            return View(new CargaEmpleados());
+        }
+        
+        [HttpPost]
+        public IActionResult SubirArchivo(CargaEmpleados model){
+
+            var datos = model.archivo;
+
+            var Nombre = Path.GetFileName(model.archivo.FileName);
+
+            var contenido = model.archivo.ContentType;
+
+            return Content(Convert.ToString(Nombre));
         }
     }
 }
