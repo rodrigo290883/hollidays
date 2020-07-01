ffd240866dd067bc5e8ae3caed3773fd9485cc14
Rodrigo Lira <rodrigo290883@gmail.com>
2020-07-01 14:28:44 -0500

    Se creo la pantalla de AdminSolicitudes

---
+++ Controllers/AdminSolicitudesClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using desconectate.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using System.Data;
+using System.Net;
+
+namespace desconectate.Controllers
+{
+
+    public class AdminSolicitudesController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AdminSolicitudesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
