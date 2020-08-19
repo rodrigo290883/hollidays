520ec3dd0850f176c758550d4c3acf59140eed68
Rodrigo Lira <rodrigo290883@gmail.com>
2020-08-19 17:20:45 -0500

    Se agrega reporteador

---
+++ Controllers/ReporteadorController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using desconectate.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace desconectate.Controllers
+{
+    public class ReporteadorController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public ReporteadorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+
+            string connString = _configuration.GetConnectionString("MyConnection"); // Read the connection string from the web.config file
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM creportes", conn);
+
+                SqlDataReader sqlReader = cmd.ExecuteReader();
+
+                List<Reporte> lst = new List<Reporte>();
+
+                while (sqlReader.Read())
+                {
+                    lst.Add(new Reporte { id_reporte = sqlReader.GetInt32(0), reporte = sqlReader[1].ToString() });
+                }
+
+                ViewBag.reportes = lst;
+
+                return View();
+            }
+        }
+
+        public DataSet ConsultaReporte(Reporte reporte)
+        {
+            DataSet data = new DataSet();
+
+            string connString = _configuration.GetConnectionString("MyConnection"); // Read the connection string from the web.config file
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT reporte,consulta FROM creportes WHERE id_reporte = @id_reporte", conn);
+                cmd.Parameters.AddWithValue("@id_reporte", reporte.id_reporte);
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                adapter.Fill(data);
+
+                return data;
+            }
+        }
+    }
+}
---
+++ Models/Reporte.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace desconectate.Models
+{
+    public class Reporte
+    {
+        public int id_reporte { get; set; }
+        public string reporte { get; set; }
+        public string consulta { get; set; }
+    }
+}
