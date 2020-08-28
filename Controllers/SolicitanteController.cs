595c00c93948bcab69922d2b7b58c06f5614289e
Rodrigo Lira <rodrigo290883@gmail.com>
2020-08-28 09:53:48 -0500

    Se corrigio la consulta de ultimo desconecte

--- Controllers/SolicitanteController.cs
+++ Controllers/SolicitanteController.cs
@@ -69,8 +69,8 @@ namespace desconectate.Controllers
 
                     SqlCommand cmd2 = new SqlCommand("SELECT TOP 1 disponibles, caducidad , periodo , (dias - disponibles) as tomados," +
                         " DATEDIFF(month, (SELECT TOP 1 fecha_inicio FROM solicitudes  WHERE estatus = 1 and fecha_inicio <= GETDATE() ORDER BY fecha_inicio DESC), GETDATE()) meses_ultimo," +
-                        "(SELECT TOP 1 fecha_inicio FROM solicitudes  WHERE estatus = 1 and fecha_inicio <= GETDATE() ORDER BY fecha_inicio DESC) as ultimo" +
-                        " FROM registros_dias WHERE idsap = @idsap  and registro_padre = 0 and caducidad >= GETDATE() ORDER BY fecha_creacion", conn);
+                        "ISNULL((SELECT TOP 1 fecha_inicio FROM solicitudes  WHERE idsap = @idsap  and estatus = 1 and fecha_inicio <= GETDATE() ORDER BY fecha_inicio DESC),e.ultimo_desconecte) as ultimo" +
+                        " FROM registros_dias r LEFT JOIN empleados e ON r.idsap = e.idsap WHERE r.idsap = @idsap  and registro_padre = 0 and caducidad >= GETDATE() ORDER BY fecha_creacion", conn);
 
                     cmd2.Parameters.AddWithValue("@idsap", usuario);
 
