651cce3a8ece9c01eee54489a6d13fe247209159
Rodrigo Lira <rodrigo290883@gmail.com>
2021-01-21 10:43:57 -0600

    Se agregaron las pantallas de extraccion de datos, permiten exportar los datos de la plataforma

---
+++ Controllers/ExtaccionController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using desconectate.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using CsvHelper;
+using CsvHelper.TypeConversion;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace desconectate.Controllers
+{
+    public class ExtraccionController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public ExtraccionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();           
+        }
+
+        public List<Registro> ConsultaReporte(string fecha_inicio, string fecha_fin)
+        {
+            List<Registro> records = new List<Registro>();
+
+            string connString = _configuration.GetConnectionString("MyConnection"); // Read the connection string from the web.config file
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select e.idsap,e.nombre,e.area,s.fecha_inicio,s.fecha_fin,t.solicitud,es.descripcion " + 
+                "from solicitudes s LEFT JOIN empleados e ON s.idsap = e.idsap LEFT JOIN ctipos_solicitud t ON s.tipo_solicitud = t.id_tipo_solicitud LEFT JOIN cestatus es ON s.estatus = es.estatus "+
+                "WHERE s.estatus = 1 and ((CONVERT(date,@fecha_inicio) between fecha_inicio and fecha_fin) or (CONVERT(date,@fecha_fin) between fecha_inicio and fecha_fin))", conn);
+                cmd.Parameters.AddWithValue("@fecha_inicio", fecha_inicio);
+                cmd.Parameters.AddWithValue("@fecha_fin", fecha_fin);
+
+                SqlDataReader sqlReader = cmd.ExecuteReader();
+
+                while (sqlReader.Read())
+                {
+                    records.Add(new Registro { IDSAP = sqlReader[0].ToString(), NOMBRE = sqlReader[1].ToString(), AREA = sqlReader[2].ToString(), FECHA_INICIO = sqlReader[3].ToString(), FECHA_FIN = sqlReader[4].ToString(), MOTIVO = sqlReader[5].ToString(), ESTATUS = sqlReader[6].ToString() });
+                }
+                
+                sqlReader.Close();
+
+                using (var writer = new StreamWriter("extraccion.csv"))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteHeader<Registro>();
+                    csv.NextRecord();
+
+                    foreach (Registro  record in records)
+                    {
+                        csv.WriteRecord(record);
+                        csv.NextRecord();
+                    }
+                }
+
+                return records;
+            }
+        }
+
+        public class Registro
+        {
+            public string IDSAP { get; set; }
+            public string NOMBRE { get; set; }
+            public string AREA { get; set; }
+            public string FECHA_INICIO { get; set; }
+            public string FECHA_FIN { get; set; }
+            public string MOTIVO { get; set; }
+            public string ESTATUS { get; set; }
+        }
+
+
+        public ActionResult DescargaCsv()
+        {
+
+            WebClient cliente = new WebClient();
+
+            string idsap = HttpContext.Session.GetString("usuario");
+            try
+            {
+
+                byte[] archivo = cliente.DownloadData("extraccion.csv");
+                return File(archivo, "text/csv", "solicitudes.csv");
+            }
+            catch (Exception ex)
+            {
+                return Content("No se encontro el archivo de poliza, favor de contactar a recursos humanos.");
+            }
+
+        }
+    }
+}
---
+++ Controllers/ExtraeUsuariosController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using desconectate.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using CsvHelper;
+using CsvHelper.TypeConversion;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace desconectate.Controllers
+{
+    public class ExtraeUsuariosController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public ExtraeUsuariosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<Registro> ConsultaReporte()
+        {
+            List<Registro> records = new List<Registro>();
+
+            string connString = _configuration.GetConnectionString("MyConnection"); // Read the connection string from the web.config file
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select e.idsap,e.nombre,e.email,e.contrasena,CONVERT(date,e.fecha_ingreso_grupo) as fecha_ingreso_grupo ,CONVERT(date,e.fecha_ingreso_uen) as fecha_ingreso_uen ,e.idsap_padre as idsap_line,e.nombre_line,e.esquema,e.rol,rd.periodo,rd.disponibles " +
+                    "from empleados e left join registros_dias rd on e.idsap = rd.idsap and rd.registro_padre = 0 and rd.caducidad >= getdate() and rd.disponibles != 0  WHERE estatus != 2", conn);
+                
+
+                SqlDataReader sqlReader = cmd.ExecuteReader();
+
+                while (sqlReader.Read())
+                {
+                    records.Add(new Registro { IDSAP = sqlReader[0].ToString(), NOMBRE = sqlReader[1].ToString(), EMAIL = sqlReader[2].ToString(), CONTRASENA = sqlReader[3].ToString(), FECHA_INGRESO_GRUPO = sqlReader[4].ToString(), FECHA_INGRESO_UEN = sqlReader[5].ToString(), IDSAP_LINE = sqlReader[6].ToString(), NOMBRE_LINE = sqlReader[7].ToString(), ESQUEMA = sqlReader[8].ToString(), ROL = sqlReader[9].ToString(), PERIODO = sqlReader[10].ToString(), DISPONIBLES = sqlReader[11].ToString() });
+                }
+
+                sqlReader.Close();
+
+                using (var writer = new StreamWriter("extraccion_usuarios.csv"))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteHeader<Registro>();
+                    csv.NextRecord();
+
+                    foreach (Registro record in records)
+                    {
+                        csv.WriteRecord(record);
+                        csv.NextRecord();
+                    }
+                }
+
+                return records;
+            }
+        }
+
+        public class Registro
+        {
+            public string IDSAP { get; set; }
+            public string NOMBRE { get; set; }
+            public string EMAIL { get; set; }
+            public string CONTRASENA { get; set; }
+            public string FECHA_INGRESO_GRUPO { get; set; }
+            public string FECHA_INGRESO_UEN { get; set; }
+            public string IDSAP_LINE { get; set; }
+            public string NOMBRE_LINE { get; set; }
+            public string ESQUEMA { get; set; }
+            public string ROL { get; set; }
+            public string PERIODO { get; set; }
+            public string DISPONIBLES { get; set; }
+        }
+
+
+        public ActionResult DescargaCsv()
+        {
+
+            WebClient cliente = new WebClient();
+
+            string idsap = HttpContext.Session.GetString("usuario");
+            try
+            {
+
+                byte[] archivo = cliente.DownloadData("extraccion_usuarios.csv");
+                return File(archivo, "text/csv", "usuarios.csv");
+            }
+            catch (Exception ex)
+            {
+                return Content("No se encontro el archivo de poliza, favor de contactar a recursos humanos.");
+            }
+
+        }
+    }
+}
